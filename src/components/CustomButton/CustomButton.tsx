77c812b736af4af413b8aaab30fc2af5e0506999
GonzaCoding <iglesias.gh@gmail.com>
2021-01-31 19:34:27 -0300

    updated CustomButton styles

--- src/components/CustomButton/CustomButton.tsx
+++ src/components/CustomButton/CustomButton.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import {View, Button} from 'react-native';
+import {View, Button, TouchableOpacity, Text} from 'react-native';
+import {styles} from './styles';
 
-const CustomButton = ({label}) => {
+const CustomButton = ({label, skin}) => {
   return (
     <View>
-      <Button title={label} />
+      <TouchableOpacity style={styles.normal}>
+        <Text style={styles.normalText}>{label}</Text>
+      </TouchableOpacity>
     </View>
   );
 };
--- src/components/CustomButton/styles.ts
+++ src/components/CustomButton/styles.ts
@@ -1,3 +1,17 @@
 import {StyleSheet} from 'react-native';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  normal: {
+    backgroundColor: 'white',
+    color: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 48,
+    borderRadius: 10,
+    elevation: 10,
+  },
+  normalText: {
+    color: 'black',
+    fontSize: 18,
+  },
+});
