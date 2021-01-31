ec0a91493acb5ec9e56e45f52964e5c08749c534
GonzaCoding <iglesias.gh@gmail.com>
2021-01-31 19:33:04 -0300

    add html-entities to unescape api response

--- src/services/QuizService.ts
+++ src/services/QuizService.ts
@@ -1,4 +1,5 @@
 import {QuestionType} from '../components/Question/type';
+import {decode} from 'html-entities';
 
 export function fetchQuiz(): Promise<QuestionType[]> {
   return new Promise<QuestionType[]>((resolve, reject) => {
@@ -6,9 +7,26 @@ export function fetchQuiz(): Promise<QuestionType[]> {
       fetch('https://opentdb.com/api.php?amount=10')
         .then((response) => response.json())
         .then((data) => {
-          resolve(data.results);
+          let questions: QuestionType[] = [];
+
+          data.results.map((results: QuestionType) => {
+            let question: QuestionType = {
+              category: decode(results.category),
+              type: results.type,
+              difficulty: decode(results.difficulty),
+              question: decode(results.question),
+              correct_answer: decode(results.correct_answer),
+              incorrect_answers: results.incorrect_answers.map((answer) =>
+                decode(answer),
+              ),
+            };
+
+            questions.push(question);
+          });
+          resolve(questions);
         });
     } catch (error) {
+      console.warn(error);
       reject(error);
     }
   });
