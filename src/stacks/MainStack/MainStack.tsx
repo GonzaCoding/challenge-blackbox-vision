fb2deb3cd7e0498ddc7c6b9428fd6a4c75f64e17
GonzaCoding <iglesias.gh@gmail.com>
2021-02-13 16:45:17 -0300

    add Score

--- src/stacks/MainStack/MainStack.tsx
+++ src/stacks/MainStack/MainStack.tsx
@@ -1,26 +1,57 @@
 import React, {useState, useEffect} from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 import Question from '../../components/Question';
+import Score from '../../components/Score';
 import {QuestionType} from '../../components/Question/type';
 import {fetchQuiz} from '../../services/QuizService';
 
 const MainStack = () => {
   const [currentQuestion, setCurrentQuestion] = useState<number>(0);
   const [quiz, setQuiz] = useState<QuestionType[]>([]);
+  const [score, setScore] = useState<number>(0);
 
   useEffect(() => {
+    getQuiz();
+  }, []);
+
+  const getQuiz = () => {
     fetchQuiz().then((questions) => {
       setQuiz(questions);
     });
-  }, []);
+  };
+
+  const onAnswer = (choice: string) => {
+    //calculate score
+    if (quiz[currentQuestion].correct_answer === choice) {
+      let points: number = 0;
+      quiz[currentQuestion].type === 'multiple' ? (points = 10) : (points = 5);
+
+      setScore((prevScore) => prevScore + points);
+    }
+
+    // set new question or show score
+    if (currentQuestion < 10) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      //console.warn(score);
+    }
+  };
 
   if (quiz.length === 0) {
     return null;
   }
 
+  if (currentQuestion === 10) {
+    return <Score score={score} />;
+  }
+
   return (
     <View>
-      <Question question={quiz[currentQuestion]} index={currentQuestion + 1} />
+      <Question
+        question={quiz[currentQuestion]}
+        index={currentQuestion + 1}
+        onAnswer={onAnswer}
+      />
     </View>
   );
 };
